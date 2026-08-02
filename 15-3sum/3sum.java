b8157c9642e39f033a1949b9b22d5883a0994a03
prrssshhhh <prashantbhanage717@gmail.com>
2026-08-02 10:46:08 +0530

    Time: 33 ms (73.37%) | Memory: 60.6 MB (5.71%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,23 +1,51 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n= nums.length;
-
-        HashSet<List<Integer>> ans = new HashSet<>();
-
-        for(int i = 0; i<n; i++){
-            HashSet<Integer> seen = new HashSet<>();
-            for(int j = i+1; j<n; j++){
-                int third = -(nums[i] +nums[j]);
-
-                if(seen.contains(third)){
-                    List<Integer> triplet = Arrays.asList(nums[i],nums[j], third);
-
-                    Collections.sort(triplet);
-                    ans.add(triplet);
-                }
-                seen.add(nums[j]);
+        List<List<Integer>> res = new ArrayList<>();
+        //sort first
+        Arrays.sort(nums);
+        //loop through fix the first nummber
+        for(int i = 0; i<nums.length-2; i++){
+            if(i>0 && nums[i] == nums[i-1]){
+                continue;
             }
+
+            int l = i+1;
+            int r = nums.length-1;
+
+            //two pointer search for remaining 2 numbers
+
+            while(l<r){
+                int sum = nums[i] + nums[l] + nums[r];
+
+                if(sum == 0){
+                    //Add the valid triplet to our result list
+
+                    List<Integer> tri = new ArrayList<>();
+                    tri.add(nums[i]);
+                    tri.add(nums[l]);
+                    tri.add(nums[r]);
+                    res.add(tri);
+                    
+                    //move the pointers inward to find the next unique numbers
+                    l++;
+                    r--;
+
+                    //skip identical left numbers
+                    while(l<r && nums[l]== nums[l-1]){
+                        l++;
+                    }
+
+                    //skip identical right
+                    while(l<r && nums[r] == nums[r+1]){
+                        r--;
+                    }
+                }else if(sum<0){
+                        l++;
+                    }else{
+                        r--;
+                    }
+                }
+            }
+            return res;
         }
-        return new ArrayList<>(ans);
     }
-}
