2d8159c51270389c114cbde1209647cd9f01c71b
prrssshhhh <prashantbhanage717@gmail.com>
2026-08-17 11:40:06 +0530

    Create reversearray.java

---
+++ An array is a data structure that stores elements of the same type in a contiguous block of memory. In an array, , of size , each memory location has some unique index,  (where ), that can be referenced as  or .  Your task is to reverse an array of integers.  Note: If you've already solved our C++ domain's Arrays Introduction challenge, you may want to skip this.  Example  Return .  Function Description  Complete the function  with the following parameter(s):  : the array to reverse Returns  : the reversed array Input Format  The first line contains an integer, , the number of integers in . The second line contains  space-separated integers that make up ./reversearray.java
@@ -0,0 +1,11 @@
+class Result {
+
+    public static List<Integer> reverseArray(List<Integer> a) {
+    Collections.reverse(a);
+
+    return (a);
+    }
+
+}
+
+//used built in function called Collections.reverse();
