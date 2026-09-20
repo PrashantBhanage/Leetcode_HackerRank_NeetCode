e6fa910bb2975b9c45b3aed37329704380e9b8d1
prrssshhhh <prashantbhanage717@gmail.com>
2026-09-20 21:06:16 +0530

    Time: 60 ms (5.00%) | Memory: 48.6 MB (9.34%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -3,7 +3,7 @@ class Solution {
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
-        // Count frequencies
+        // Count frequencies//
         for (int i = 0; i < nums.length; i++) {
             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
         }
